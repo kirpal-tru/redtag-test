54360aa79098f48447ce79c507462aa8e66be232
kirpal-tru <51973101+tru-team@users.noreply.github.com>
2026-04-24 12:23:29 +0530

    Contentful

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import { client } from '@/lib/contentful';
 
-export default function Home() {
+async function getHome() {
+  const response = await client.getEntries({
+    content_type: 'home', // Replace with your actual Content Type ID
+  });
+
+  return response.items.map((item) => ({
+    title: item.fields.title as string
+  }));
+}
+
+export default async function Home() {
+
+  const home = await getHome();
+ 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -12,10 +26,15 @@ export default function Home() {
           height={20}
           priority
         />
+        {home.map((post) => (
+            <div key={post.slug} className="border p-4 rounded shadow">
+              <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">{post.title}</p>
+            </div>
+          ))}
+          
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+          
+          
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
