d7ba30f0a03424e242b45325960fdbd063eb38f3
kirpal-tru <51973101+tru-team@users.noreply.github.com>
2026-04-24 17:01:41 +0530

    Fixes

--- components/HomePreview.tsx
+++ components/HomePreview.tsx
@@ -10,9 +10,9 @@ export default function HomePreview({ initialData }: { initialData: { title: str
   useEffect(() => {
     ContentfulLivePreview.init({
       locale: 'en-US', // This is required and allows you to set the locale once and have it reused throughout the preview
-      enableInspectorMode: false, // This allows you to toggle the inspector mode which is on by default
-      enableLiveUpdates: false, // This allows you to toggle the live updates which is on by default
-      debugMode: false, // This allows you to toggle the debug mode which is off by default
+      enableInspectorMode: true, // This allows you to toggle the inspector mode which is on by default
+      enableLiveUpdates: true, // This allows you to toggle the live updates which is on by default
+      debugMode: true, // This allows you to toggle the debug mode which is off by default
       targetOrigin: 'https://app.contentful.com', // This allows you to configure the allowed host of the live preview (default: ['https://app.contentful.com', 'https://app.eu.contentful.com'])
     });
   }, []);
