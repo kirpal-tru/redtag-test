e7a966b3287b07f6ecc42ef05f87b18fecc508a3
kirpal-tru <51973101+tru-team@users.noreply.github.com>
2026-04-24 13:30:20 +0530

    Fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: [
+    '192.168.10.120',
+    'app.contentful.com',
+    'app.eu.contentful.com',
+    'https://redtag-test.vercel.app',
+    'local-origin.dev', 
+    '*.local-origin.dev',
+    '*.vercel.app',
+  ],
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
