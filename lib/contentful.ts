38384589f7dce6b427b61d0bdb37211111498ac2
kirpal-tru <107383280+kirpal-tru@users.noreply.github.com>
2026-04-27 13:07:01 +0530

    Update contentful.ts

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'contentful';
 
 export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
   host: 'preview.contentful.com',
 });
